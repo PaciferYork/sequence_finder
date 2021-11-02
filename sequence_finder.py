13a52815cb474d99c496459bf487e82a12a27621
Kunal <anand.kunal2007@gmail.com>
2021-11-02 10:14:23 +0530

    initial commit

---
+++ sequence_finder.py
@@ -0,0 +1,26 @@
+'''
+Question-
+make a python program taking users input from user and give him/her output of the sequence of the number provided
+upto numbers excluding numbers.
+'''
+
+print("Enter the number you want to find sequence of-")
+primarynumber = int(input())
+print("The Number undertaken by you is,", primarynumber , ".")
+
+print("5 multiples of ", primarynumber)
+# multiple 1
+print("1.\t", primarynumber*2)
+print("2.\t", primarynumber*3)
+print("3.\t", primarynumber*4)
+print("4.\t", primarynumber*5)
+print("5.\t", primarynumber*6)
+
+print("Thanks for Trying.")
+print("PaciferYork Inc. © \n By- Kunal Anand.")
+
+
+# multiple 2
+# multiple 3
+# multiple 4
+# multiple 5
